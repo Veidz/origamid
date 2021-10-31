f4afee14dd55f84d897f8130e17d1d356f154884
obastosleandro <bastosleandrodesigner@gmail.com>
2021-10-31 19:45:04 -0300

    exercicio/1

--- src/App.js
+++ src/App.js
@@ -24,15 +24,19 @@ const mario = {
 };
 
 const App = () => {
-  const dados = luana;
-
+  const dados = mario;
+  const gastos = dados.compras.reduce((acc, p) => {
+    return Number(p.preco.replace('R$ ', '')) + acc
+  }, 0);
   return (
     <>
-    <p>{`Nome: ${dados.cliente}`}</p>
-    <p>{`Idade: ${dados.idade}`}</p>
-    <p>Situação: <span style={{color: dados.ativa ? 'green' : 'red'}}>{`${dados.ativa ? 'Ativa' : 'Inativa'}`}</span></p>
+      <p>{`Nome: ${dados.cliente}`}</p>
+      <p>{`Idade: ${dados.idade}`}</p>
+      <p>Situação: <span style={{ color: dados.ativa ? 'green' : 'red' }}>{`${dados.ativa ? 'Ativa' : 'Inativa'}`}</span></p>
+      <p className="gastos">Total gasto: {gastos}</p>
+      <p>{gastos > 10000 ? 'Você está gastando demais' : ''}</p>
     </>
   );
-};
+}
 
 export default App;
